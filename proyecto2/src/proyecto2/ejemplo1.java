ef32cad789735c1dfd37296f41f0cd854038e2fa
sandragallego <sandragallego@172.20.10.2>
2022-11-29 21:32:10 +0100

    Adios

--- proyecto2/src/proyecto2/ejemplo1.java
+++ proyecto2/src/proyecto2/ejemplo1.java
@@ -3,7 +3,15 @@ package proyecto2;
 public class ejemplo1 {
 
 	public static void main(String[] args) {
+<<<<<<< HEAD
 		System.out.println("HELLOU mundo nnn");
+=======
+		System.out.println("Hola mundo");
+		
+		public static void imprimeSaludo() { 
+System.out.println(“Hei”);	
+}
+>>>>>>> branch 'master' of https://github.com/sandraG95/Boletin1.git
 
 	}
 
